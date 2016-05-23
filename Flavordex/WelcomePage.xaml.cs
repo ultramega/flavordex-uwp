9cb9f22605f2ac51bc3f4a08c56aa06a7409d15e
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-05-23 13:29:48 -0700

    add content to the welcome page

--- Flavordex/WelcomePage.xaml.cs
+++ Flavordex/WelcomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Controls;
 
 namespace Flavordex
 {
@@ -7,6 +9,34 @@ namespace Flavordex
     /// </summary>
     public sealed partial class WelcomePage : Page
     {
+        /// <summary>
+        /// Gets the version message.
+        /// </summary>
+        private string Version
+        {
+            get
+            {
+                var format = ResourceLoader.GetForCurrentView().GetString("Message/Version");
+                var version = Windows.ApplicationModel.Package.Current.Id.Version;
+                return string.Format(format, version.Major, version.Minor, version.Build, version.Revision);
+            }
+        }
+
+        /// <summary>
+        /// Gets the copyright message.
+        /// </summary>
+        private string Copyright
+        {
+            get
+            {
+                var format = ResourceLoader.GetForCurrentView().GetString("Message/Copyright");
+                return string.Format(format, DateTime.Now.Year);
+            }
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public WelcomePage()
         {
             InitializeComponent();
