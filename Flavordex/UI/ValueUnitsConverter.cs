e1efcf0cfad6fa929c94f4f980df622615a1dc87
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-06-06 13:04:51 -0700

    check for empty strings in the value units converter

--- Flavordex/UI/ValueUnitsConverter.cs
+++ Flavordex/UI/ValueUnitsConverter.cs
@@ -18,7 +18,7 @@ namespace Flavordex.UI
         /// <returns>The value with the parameter appended.</returns>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (parameter is string && value != null)
+            if (parameter is string && value != null && !string.IsNullOrWhiteSpace(value.ToString()))
             {
                 return string.Format("{0}{1}", value, parameter);
             }
